0586738d0bd23194e93a3c03f1b3ff7c59e8d8b1
ehseezed <97066152+ehseezed@users.noreply.github.com>
2025-08-24 21:32:19 -0500

    c

--- worlds/manual_bubblebobblepasswordblitz_sensokaeru/hooks/World.py
+++ worlds/manual_bubblebobblepasswordblitz_sensokaeru/hooks/World.py
@@ -17,9 +17,6 @@ from ..Helpers import is_option_enabled, get_option_value, format_state_prog_ite
 # calling logging.info("message") anywhere below in this file will output the message to both console and log file
 import logging
 
-from ...tloz.Items import progression
-
-
 ########################################################################################
 ## Order of method calls when the world generates:
 ##    1. create_regions - Creates regions and locations
