d8d3f864b7713e1acf15d6cea382713eb22da3ff
ehseezed <97066152+ehseezed@users.noreply.github.com>
2025-10-16 15:23:01 -0500

    Im silly

--- Options.py
+++ Options.py
@@ -1755,7 +1755,6 @@ def generate_yaml_templates(target_folder: typing.Union[str, "pathlib.Path"], ge
 
             res = template.render(
                 option_groups=option_groups,
-                __version__=__version__,
                 game=game_name,
                 world_version=world.world_version.as_simple_string(),
                 yaml_dump=yaml_dump_scalar,
